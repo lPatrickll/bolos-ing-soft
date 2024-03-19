a6625e9b3625212beb2ac17b56e711caa8063c03
Patrick <patrickgonzalesquintana@gmail.com>
2024-03-19 00:47:32 -0400

    Se factorizo el codigo de los test

--- src/Bolos.spec.js
+++ src/Bolos.spec.js
@@ -1,19 +1,20 @@
 import Game from "./claseGame";
 
+const g = new Game();
+
+function rollMany(n, pins){
+  for (let i = 0; i < n; i++)
+    g.roll(pins);
+}
+
 describe("Bolos", () => {
   it("deberia mostrar 0", () => {
-    const g = new Game();
-    for (let i = 0; i < 20; i++){
-      g.roll(0);
-    }
+    rollMany(20, 0);
     expect(g.score()).toEqual(0);
   });
 
   it("deberia mostrar el score 20", () => {
-    const g = new Game();
-    for (let i = 0; i < 20; i++){
-      g.roll(1);
-    }
+    rollMany(20, 1);
     expect(g.score()).toEqual(20);
   });
 });
