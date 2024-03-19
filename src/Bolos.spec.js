9b7156a48963e8d96dfbe922348bbc5c4b907f3b
Patrick <patrickgonzalesquintana@gmail.com>
2024-03-19 01:46:37 -0400

    Se factorizo el codigo de los test para poder pasar las pruebas

--- src/Bolos.spec.js
+++ src/Bolos.spec.js
@@ -1,11 +1,17 @@
 import Game from "./claseGame";
 
-const g = new Game();
+let g;
 
-function rollMany(n, pins){
-  for (let i = 0; i < n; i++)
+beforeEach(() => {
+  g = new Game();
+});
+
+function rollMany(n, pins) {
+  for (let i = 0; i < n; i++) {
     g.roll(pins);
+  }
 }
+
 
 describe("Bolos", () => {
   it("deberia mostrar 0", () => {
@@ -17,4 +23,12 @@ describe("Bolos", () => {
     rollMany(20, 1);
     expect(g.score()).toEqual(20);
   });
+
+  /*it("deberia mostrar el score 16", () => {
+    g.roll(5);
+    g.roll(5); // spare
+    g.roll(3);
+    rollMany(17, 0);
+    expect(g.score()).toEqual(16);
+  });*/
 });
--- src/claseGame.js
+++ src/claseGame.js
@@ -1,14 +1,22 @@
 class Game {
     constructor() {
         this.totalScore = 0;
+        this._rolls = new Array(21).fill(0);
+        this._currentRoll = 0;
     }
 
     roll(pins) {
-        this.totalScore += pins;
+        this._rolls[this._currentRoll++] = pins;
     }
 
     score() {
-        return this.totalScore;
+        let score = 0;
+        let i = 0;
+        for (let frame = 0; frame < 10; frame++) {
+            score += this._rolls[i] + this._rolls[i + 1];
+            i += 2;
+        }
+        return score;
     }
 }
 
